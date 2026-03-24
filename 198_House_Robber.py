a67b90fe5430a8d71a5dc0e412e35fea811f8a80
Kiran-8848 <kiranshetty1030@gmail.com>
2026-03-24 10:24:41 +0530

    added optimized code in the probelm

--- 198_House_Robber.py
+++ 198_House_Robber.py
@@ -1,3 +1,4 @@
+#Top down approach
 class Solution:
     def rob(self, nums: List[int],) -> int:
         ans=0
@@ -6,7 +7,21 @@ class Solution:
 
             if ind>=len(nums):
                 return 0
+            if ind in dp :return dp[ind]
+            #skip
             skip=f(nums,ind+1)
             take=nums[ind]+f(nums,ind+2)
+            dp[ind] =max(skip, take)
             return max(skip,take)
         return f(nums, 0)
+#bottom up apporoch
+class Solution:
+    def rob(self, nums: List[int],) -> int:
+        if len(nums)==1: return nums[0]
+        ans=[0]*len(nums)
+        ans[0] = nums[0]
+        ans[1]= max(nums[0], nums[1])
+        for i in range (2, len(nums)):
+            ans[i] = max(ans[i-1], nums[i]+ans[i-2])
+        return ans[len(nums)-1]
+         
