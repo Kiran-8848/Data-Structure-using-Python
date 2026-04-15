2ddbb3030e7cdc7301e598058c76d4c83bf8d92e
Kiran-8848 <kiranshetty1030@gmail.com>
2026-04-15 10:20:44 +0530

    solved using simple logic of low high

---
+++ 98_Validate_Binary_SearchTree.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+# class Solution:
+    # def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # def validate(node,low,left,right,high):
+            # if not node:
+                # return True
+            # if not (left<node.val<right):
+                # return False
+            # return (validate(node.left, low, node.val) and
+                    # validate(node.right, node.val, high))
+        # return validate(root, float('-inf'), float('inf'))
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+        def validate(node, low, high):
+            if not node:
+                return True
+            
+            # Check BST condition
+            if not (low < node.val < high):
+                return False
+            
+            # Recursive check
+            return (validate(node.left, low, node.val) and
+                    validate(node.right, node.val, high))
+        
+        return validate(root, float('-inf'), float('inf'))
+        
+        
